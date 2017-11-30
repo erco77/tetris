0f4dde5338f400b3cd01646a163b7caaa50364dd
Greg Ercolano <erco@seriss.com>
2017-11-30 11:59:32 -0800

    Initial code cleanup to make compile for linux with modern gcc.

--- tetris.c
+++ tetris.c
@@ -1,31 +1,31 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <signal.h>
+#include <unistd.h>
+#include <time.h>
 
-#define VERSION "1.10"
+#define VERSION "1.20"
 
-#ifdef NEVER
-**************************************************************************
- 
- TETRIS - An interactive game implemented for text screen terminals
- 
-            Rev  Date     Author          Description
-            ---- -------- --------------- ----------------------
-            1.00 05/19/92 Greg Ercolano   Initial Implementation
-            x.xx xx/xx/xx xxxx            xxxx
- 
-            Tested to compile and work on the following machines:
-                1) IBM PC
-                2) Silicon Graphics Indigo/PI/GTX
-                3) Solbourne/Sun (if you do the following:)
-                 grep -v '/*PROTOTYPE*/'<tetris.c >tetris-sun.c
- 
-**************************************************************************
-#endif
- 
-#define IBMPC 1               /* for compiling on IBM PCs only */
-/*#define UNIX    1               /* for compiling under UNIX only (SGI/Sun) */
+//#define IBMPC 1               /* for compiling on IBM PCs only */
+#define UNIX    1               /* for compiling under UNIX only (SGI/Sun) */
+
+//********************************************************************
+// 
+// TETRIS - An interactive game implemented for text screen terminals
+// 
+//            Rev  Date     Author          Description
+//            ---- -------- --------------- ----------------------
+//            1.00 05/19/92 Greg Ercolano   Initial Implementation
+//            1.20 11/29/17 Greg Ercolano   Unix support for arrow keys
+//            x.xx xx/xx/xx xxxx            xxxx
+// 
+//            Tested to compile and work on the following machines:
+//                1) IBM PC
+//                2) Silicon Graphics Indigo/PI/GTX
+//                3) Solbourne/Sun (if you do the following:)
+//                 grep -v '/*PROTOTYPE*/'<tetris.c >tetris-sun.c
+// 
+//********************************************************************
  
 /* KEYSTROKE TRANSLATIONS */
 #define DOWN   1
@@ -63,11 +63,11 @@
 #define SHAPEMAX        4       /* width/height of shape characters */
  
 /* GSCREEN PIXEL BIT PLANES */
-#define NOSHAPE                 0       /* empty space */
-#define NEWSHAPE                1       /* current moving shape */
-#define OLDSHAPE                2       /* last moving shape */
-#define OLAPSHAPE               3       /* new & last shape overlap */
-#define PETRIFIEDSHAPE          4       /* petrified into position */
+#define NOSHAPE         0       /* empty space */
+#define NEWSHAPE        1       /* current moving shape */
+#define OLDSHAPE        2       /* last moving shape */
+#define OLAPSHAPE       3       /* new & last shape overlap */
+#define PETRIFIEDSHAPE  4       /* petrified into position */
 
 #define ABS(a)          (((a)<0)?-(a):(a))
 #define SGN(a)          (((a)<0)?(-1):1)         /* binary sign of a: -1, 1 */
@@ -75,8 +75,7 @@
  
 char *term;
  
-char *Gwindow[] = 
-{
+char *Gwindow[] = {
 "\t\t  ......................               ",
 "\t\t  :                    :               ",
 "\t\t  :                    :               ",
@@ -89,22 +88,21 @@ char *Gwindow[] =
 "\t\t  :                    :               ",
 "\t\t  :                    :               ",
 "\t\t  :                    :               ",
-"\t\t  :                    :     .......... ",
-"\t\t  :                    :     :        : ",
-"\t\t  :                    :     :        : ",
-"\t\t  :                    :     :        : ",
-"\t\t  :                    :     :        : ",
-"\t\t  :                    :     :        : ",
-"\t\t  :                    :     :        : ",
-"\t\t  :                    :     :........: ",
+"\t\t  :                    :     ..........",
+"\t\t  :                    :     :        :",
+"\t\t  :                    :     :        :",
+"\t\t  :                    :     :        :",
+"\t\t  :                    :     :        :",
+"\t\t  :                    :     :        :",
+"\t\t  :                    :     :        :",
+"\t\t  :                    :     :........:",
 "\t\t  :                    :               ",
-"\t\t  :....................:     Rows =     ",
+"\t\t  :....................:     Rows =    ",
 NULL
 };
  
 /* SHAPE TABLES (Indexing: [shape] [y] [rotation] [x]) */
-char *Gshapes[MAXSHAPES+1][4][4] =              /* shape icons */
-{
+char *Gshapes[MAXSHAPES+1][4][4] = {            /* shape icons */
   {
     { "    ", "    ", "    ", "    " },
     { " ## ", " ## ", " ## ", " ## " },
@@ -140,7 +138,7 @@ char *Gshapes[MAXSHAPES+1][4][4] =              /* shape icons */
     { " ## ", " ## ", " ## ", " ## " },
     { "    ", " #  ", "    ", " #  " },
     { "    ", "    ", "    ", "    " },
-    }
+  }
 };
  
 /* GLOBAL VARIABLES */
@@ -154,11 +152,10 @@ int Gnextshape,                         /* the next shape coming */
 char Gscreen[GAMEHEIGHT][GAMEWIDTH];    /* game screen's memory space */
  
 /* CLEAR THE TERMINAL SCREEN */
-ClearScreen()
+void ClearScreen()
 {
     if (term)
-        if (term[0]=='w' && term[1]=='y')       /* WYSE TERMINALS */
-        {
+        if (term[0]=='w' && term[1]=='y') {     /* WYSE TERMINALS */
             printf("%c%c%c\r",0x1b,0x28,0x1a);  /* CLEAR */
             printf("%c",0x1e);                  /* HOME */
             /* printf("%c%c%c%c",0x1b,0x3d,0x20,0x20); */
@@ -174,18 +171,11 @@ void LocateXY(int,int); /*PROTOTYPE*/
 void LocateXY(x,y)
     int x,y;            /* x: 1-80, y:1-24 */
 {
-    if (term)
-    {
-        if (term[0]=='w')          /* WYSE TERMINALS */
-        {
-            /* WYSE CURSOR POSITION */
-            printf("%c%c%c%c",0x1b, 0x3d, 0x20+y-1, 0x20+x-1);
-            return;
-        }
-    } 
- 
-    /* CURSOR POSITION FOR VT100/IRIS-ANSI/IBMPC ANSI TERMINALS */
-    printf("\33[%d;%dH",y,x);
+    if (term && term[0]=='w') {			/* WYSE TERMINALS */
+	printf("%c%c%c%c",0x1b, 0x3d, 0x20+y-1, 0x20+x-1);
+    } else {
+	printf("\33[%d;%dH",y,x);		/* VT100/IBMPC/etc */
+    }
 }
  
 /* DRAW A PIXEL ON THE TERMINAL AT CURRENT CURSOR POSITION */
@@ -198,11 +188,10 @@ void DrawPixel(on)
 }
  
 /* DRAW THE SHAPE THAT IS 'on deck' IN THE RIGHT HAND BOX */
-DrawOnDeck()
+void DrawOnDeck()
 {
     int x,y;
-    for (y=0; y<SHAPEMAX; y++)
-    {
+    for (y=0; y<SHAPEMAX; y++) {
         LocateXY(DECKXOFFSET,y+DECKYOFFSET+1);
         for (x=0; x<SHAPEMAX; x++)
             DrawPixel ((Gshapes[Gnextshape][y][0][x]=='#')?1:0);
@@ -214,8 +203,7 @@ void UpdateScore(int);  /*PROTOTYPE*/
 void UpdateScore(force)
     int force;
 {
-    if (force || Grows!=Glastrows)
-    {
+    if (force || Grows!=Glastrows) {
         LocateXY(54,22);
         printf("%d ",Grows);
         Glastrows = Grows;
@@ -242,48 +230,36 @@ void Redraw(all)
     int all;    /* redraw everything */
 {
     int x,y,flags;
-    if (all&WINDOW)
-    {
+    if (all&WINDOW) {
         ClearScreen();
         for (y=0; Gwindow[y]; y++)
             printf("%s\n",Gwindow[y]);
     }
-    if (all&GSCREEN)
-    {
-        for (y=0; y<GAMEHEIGHT; y++)
-        {
+    if (all&GSCREEN) {
+        for (y=0; y<GAMEHEIGHT; y++) {
             LocateXY(LEFTOFFSET,y+TOPOFFSET);
             for (x=0; x<GAMEWIDTH; x++)
                 DrawPixel((Gscreen[y][x]==NOSHAPE)?0:1);
         } 
     }
 
-    if (Gtest)
-    {
+    if (Gtest) {
         int x,y;
-
-        for (y=0; y<GAMEHEIGHT; y++)
-        {
+        for (y=0; y<GAMEHEIGHT; y++) {
             LocateXY(0,y+TOPOFFSET);
             for (x=0; x<GAMEWIDTH; x++)
                 printf("%d",Gscreen[y][x]);
         }
     }
  
-    if (all)
-    {
+    if (all) {
         UpdateScore(1);
         DrawOnDeck();
-    }
-    else
-    {
-        for (y=0; y<GAMEHEIGHT; y++)
-        {
-            for (x=0; x<GAMEWIDTH; x++)
-            {
+    } else {
+        for (y=0; y<GAMEHEIGHT; y++) {
+            for (x=0; x<GAMEWIDTH; x++) {
                 flags = Gscreen[y][x] & (OLAPSHAPE^0xffff);
-                switch(Gscreen[y][x]&OLAPSHAPE)
-                {
+                switch(Gscreen[y][x]&OLAPSHAPE) {
                     /* NEWLY DRAWN */
                     case NEWSHAPE:
                         LocateXY(x*2+LEFTOFFSET,y+TOPOFFSET);
@@ -312,7 +288,7 @@ void Redraw(all)
 }
  
 /* CLEAR THE GAME/INITIALIZE VARIABLES */
-Clear()
+void Clear()
 {
     long lt;
  
@@ -336,7 +312,6 @@ Clear()
     }
     MakeNewShape(0);     /* new shape */
     MakeNewShape(0);     /* and another on deck */
- 
     Redraw(ALL);
 }
  
@@ -352,10 +327,8 @@ Clear()
 int ReadKey();  /*PROTOTYPE*/
 int ReadKey()
 {
-    if (kbhit())
-    {
-        switch(getch())
-        {
+    if (kbhit()) {
+        switch(getch()) {
             case 0x1b: return(QUIT);
             case  'j': return(DOWN);
             case  'h': return(LEFT);
@@ -366,8 +339,7 @@ int ReadKey()
             case  ' ': return(ROTATE);
             case  'r': return(REDRAW);
             case NULL:
-                switch(getch())
-                {
+                switch(getch()) {
                     case 0x50: return(DOWN);
                     case 0x4b: return(LEFT);
                     case 0x4d: return(RIGHT);
@@ -388,6 +360,12 @@ InitTerminal() { }
 static struct termio tio,                       /* game settings */
                      tiosave;                   /* saved UNIX settings */
 
+/* RESTORE USERS'S ORIGINAL TERMINAL SETTINGS */
+void EndTerminal()
+{
+    ioctl(fileno(stdin),TCSETA,&tiosave);       /* assert old settings */
+}
+ 
 void SIGINTTrap()
 {
     signal(SIGINT, SIGINTTrap);
@@ -399,7 +377,7 @@ void SIGINTTrap()
 }
 
 /* FORCE UNIX TO READ TERMINAL KEYS NON-BUFFERED/NO ECHO */
-InitTerminal()
+void InitTerminal()
 {
     ioctl(fileno(stdin),TCGETA,&tiosave);       /* save current settings */
     ioctl(fileno(stdin),TCGETA,&tio);           /* get ready to modify */
@@ -407,13 +385,7 @@ InitTerminal()
     tio.c_cc[VMIN]  = 0;                        /* No waiting */
     tio.c_cc[VTIME] = 0;                        /* No kidding */
     ioctl(fileno(stdin),TCSETA,&tio);           /* assert new settings */
-    signal(SIGINT,  SIGINTTrap);
-}
- 
-/* RESTORE USERS'S ORIGINAL TERMINAL SETTINGS */
-EndTerminal()
-{
-    ioctl(fileno(stdin),TCSETA,&tiosave);       /* assert old settings */
+    signal(SIGINT, SIGINTTrap);
 }
  
 /* READ A SINGLE KEY (under UNIX) */
@@ -421,24 +393,47 @@ EndTerminal()
 int ReadKey(void);      /*PROTOTYPE*/
 int ReadKey()
 {
+    static int esc = 0;
     char c;
  
     if (read(fileno(stdin),&c,1)!=1)            /* read character */
-        c = 0;                                  /* none ready? return 0 */
+        return(0);                              /* none ready? return 0 */
  
-    switch(c)
-    {
-        case 0x1b: return(QUIT);
+    // Handle arrow keys -- these are 3 character sequences:
+    //    up=ESC[A, down=ESC[B, right=ESC[C, left=ESC[D
+    //
+    switch ( esc ) {
+        case 0: if ( c == 0x1b ) { esc = 1; return 0; }
+	        break;   // fall thru to normal char handling
+	case 1: if ( c == '[' ) { esc = 2; return 0; }
+	        esc = 0; // reset
+		break;   // fall thru to normal char handling
+	case 2: {
+	    int ret = 0;
+	    // LocateXY(1,1); printf("GOT '%c'\n", c);
+	    switch (c) {
+		case 'A': ret = ROTATE; break;  //    UP KEY
+		case 'B': ret = DOWN;   break;  //  DOWN KEY
+		case 'C': ret = RIGHT;  break;  // RIGHT KEY
+		case 'D': ret = LEFT;   break;  //  LEFT KEY
+	    }
+	    esc = 0;	 // last char in sequence, reset
+	    return ret;
+       }
+    }
+
+    esc = 0;
+    switch(c) {
+        case  'q': return(QUIT);
         case  'j': return(DOWN);
         case  'h': return(LEFT);
         case  'l': return(RIGHT);
         case  'p': return(PAUSE); 
         case  'z': return(TEST);
-        case  'q': return(ROTATE);
         case  ' ': return(ROTATE);
         case  'r': return(REDRAW);
     }
-    return(NULL);
+    return(0);
 }
 #endif
  
@@ -457,45 +452,43 @@ int HandleTimer(yforce)
     else              { if (yforce) *yforce = 0; return(0); }
 }
  
+/* EXIT PROGRAM WITH SCORE SHOWN */
+void Texit(int); /*PROTOTYPE*/
+void Texit(v)
+    int v;
+{
+    ClearScreen();
+    EndTerminal();
+    printf("Total rows: %d\n",Grows);
+    fflush(stdout);
+    exit(v);
+}
+
 /* HANDLE READING BUTTON EVENTS AND UPDATING POSITION VARIS */
 int HandleButtons(int*, int*, int*, int*);      /*PROTOTYPE*/
 int HandleButtons(x, y, rotate, yforce)
     int *x, *y, *rotate, *yforce;
 {
-    int c,
-        events=0;
- 
+    int c, events=0;
     /* READ ALL BUFFERED KEYSTROKES */
-    while (c=ReadKey())
-    {
-        switch(c)
-        {
+    while ((c=ReadKey())) {
+        switch(c) {
             case   LEFT: *x      -= 1; ++events; break;
             case  RIGHT: *x      += 1; ++events; break;
             case   DOWN: *y      += 1; ++events; break;
             case ROTATE: *rotate += 1; ++events; break;
             case   QUIT: Texit(1);               break;
             case  PAUSE: while (!ReadKey()) { }  break;
-            /* TESTING ONLY */
-            case   TEST: Gtest ^= 1; break;
-            /* Gshape = (Gshape + 1) % MAXSHAPES; break; */
-
+            case   TEST: Gtest ^= 1;             break;  /* testing mode */
             case REDRAW: Redraw(ALL);            break;
         }
     }
-
-    /*
-    if (*y>1) *y=1;
-    if (*x>1) *x=1;
-    */
-
     return(events);
 }
  
 #define SIDES(x,y) ((x)<0||(x)>=GAMEWIDTH)
 #define BOTT(x,y) ((y)>=GAMEHEIGHT)
 #define TOP(x,y)  ((y)<0)
- 
 #define CLIP(x,y) (TOP(x,y)||BOTT(x,y)||SIDES(x,y))
  
 /* DRAWS THE SHAPE INTO THE SCREEN ARRAY
@@ -510,16 +503,11 @@ int DrawShape(x,y,rotate,olap)
 {
     int t, r;
  
-    if (olap)
-    {
+    if (olap) {
         int err=0;
- 
-        for (t=0; t<SHAPEMAX; t++)
-        {
-            for (r=0; r<SHAPEMAX; r++)
-            {
-                if (Gshapes[Gshape][t][rotate][r]=='#')
-                {
+        for (t=0; t<SHAPEMAX; t++) {
+            for (r=0; r<SHAPEMAX; r++) {
+                if (Gshapes[Gshape][t][rotate][r]=='#') {
                     /* HIT BOTTOM? SPECIAL CASE FOR PETRIFICATION */
                     if (BOTT(x+r,y+t))
                         return(2);
@@ -538,16 +526,16 @@ int DrawShape(x,y,rotate,olap)
     }
  
     /* DRAW THE SHAPE INTO THE SCREEN ARRAY */
-    for (t=0; t<SHAPEMAX; t++)
-        for (r=0; r<SHAPEMAX; r++)
-        {
-            if (CLIP(x+r, y+t))
+    for (t=0; t<SHAPEMAX; t++) {
+        for (r=0; r<SHAPEMAX; r++) {
+            if (CLIP(x+r, y+t)) {
                 continue;
-            else
+            } else {
                 Gscreen[y+t][x+r] |=
                     (Gshapes[Gshape][t][rotate][r]=='#') ? NEWSHAPE : NOSHAPE;
-        }
- 
+            }
+	}
+    }
     return(0); 
 }
  
@@ -556,14 +544,11 @@ void PetrifyScreen(void);       /*PROTOTYPE*/
 void PetrifyScreen()
 {
     int x, y;
- 
     /* THIS PETRIFIES ANY OLD/OVERLAP DATA INTO PLACE */
     /* (ie. the last drawn shape) */
- 
     for (y=0; y<GAMEHEIGHT; y++)
         for (x=0; x<GAMEWIDTH; x++)
             Gscreen[y][x] = (Gscreen[y][x]) ? PETRIFIEDSHAPE : NOSHAPE;
- 
     MakeNewShape(1);
 }
  
@@ -571,22 +556,17 @@ void PetrifyScreen()
 void HandleCompletedRows(void);      /*PROTOTYPE*/
 void HandleCompletedRows()
 {
-    int x,y,total,trows=0,
-        rows[GAMEHEIGHT];
- 
-    for (y=0; y<GAMEHEIGHT; y++)
-    {
-        for (x=0,total=GAMEWIDTH; x<GAMEWIDTH; x++)
-        {
+    int x,y,total,trows=0, rows[GAMEHEIGHT];
+    for (y=0; y<GAMEHEIGHT; y++) {
+        for (x=0,total=GAMEWIDTH; x<GAMEWIDTH; x++) {
             if (Gscreen[y][x]==NOSHAPE) break;
             else                        --total;
         }
         if (total==0) { rows[trows++] = y; }
     }
  
-    if (trows)
-    {
-        int *a,*b,t,r,junk;
+    if (trows) {
+        int t,r,junk;
  
         /* FLASH THE ROWS */
         for (t=1; t<3; t++)
@@ -606,7 +586,6 @@ void HandleCompletedRows()
  
         Redraw(GSCREEN);
         Grows += trows;
-        /* UpdateScore(0); Updated elsewhere */
     }
  
     /* CLEAR THE KEYBOARD BUFFER */
@@ -614,18 +593,17 @@ void HandleCompletedRows()
 }
  
 /* HANDLE SHAPE DRAWING/COLLISIONS/CLIPPING */
-int HandleShape(int*,int*,int*,int*);   /*PROTOTYPE*/
-int HandleShape(x,y,rotate,yforce)
+void HandleShape(int*,int*,int*,int*);   /*PROTOTYPE*/
+void HandleShape(x,y,rotate,yforce)
     int *x, *y, *rotate, *yforce;
 {
     /* CHECK IF SHAPE OVERLAPS ANOTHER OR SCREEN EDGE */
     /* IN REQUESTED ORIENTATION                       */
- 
-    while (1)
-    {
-        switch(DrawShape(Gx + *x, Gy + *y + *yforce, (Grotate + *rotate) % 4,
-                         OLAPCHECK))
-        {
+    while (1) {
+        switch(DrawShape(Gx + *x,
+	                 Gy + *y + *yforce,
+			 (Grotate + *rotate) % 4,
+                         OLAPCHECK)) {
             case 1: /* LEFT/RIGHT EDGE COLLISION */
                 /* Undo button events until no collision */
                 if (*rotate!=0) { *rotate -= ZSGN(*rotate); continue; }
@@ -634,7 +612,6 @@ int HandleShape(x,y,rotate,yforce)
                 break;
      
             case 2: /* BOTTOM OR PETRIFIED COLLISION */
-
                 /* Undo button events to avoid collision */
                 if (ABS(*rotate)!=0) { *rotate -= ZSGN(*rotate); continue; }
                 if (ABS(*x)!=0)      { *x      -= ZSGN(*x);      continue; }
@@ -656,11 +633,10 @@ int HandleShape(x,y,rotate,yforce)
     Gx      += *x;
     Gy      += (*y + *yforce);
     Grotate += *rotate;
- 
     DrawShape(Gx, Gy, (Grotate % 4), DRAWSHAPE);
 }
  
-main()
+int main()
 {
     int x,y,rotate,yforce;
  
@@ -673,29 +649,16 @@ main()
     term = getenv("TERM");
  
     Clear();
-    while (1)
-    {
+    while (1) {
         x = y = rotate = yforce = 0;
  
         HandleTimer(&yforce);           /* forces piece down */
         HandleButtons(&x,&y,&rotate,&yforce);
-        if (x||y||rotate||yforce)
-        {
+        if (x||y||rotate||yforce) {
             HandleShape(&x,&y,&rotate,&yforce);
             Redraw(CHANGED);    /* redraw only if something changed */
         }
     }
+    return 0;
 }
  
-/* EXIT PROGRAM WITH SCORE SHOWN */
-int Texit(int); /*PROTOTYPE*/
-int Texit(v)
-    int v;
-{
-    ClearScreen();
-    EndTerminal();
-    printf("Total rows: %d\n",Grows);
-    fflush(stdout);
-    exit(v);
-}
-
